af436d759947dd1b4506b7cbfccd4a265e5ee0e9
Alan Delgado <7a.alan@gmail.com>
2024-09-01 23:28:07 -0600

    Add keybinds, remove autofocus to input

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function updateClock() {
 }
 
 updateClock();
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'y' || event.key === 'Y') {
+        window.location.href = 'https://www.youtube.com';
+    }
+    if (event.key === 't' || event.key === 'T') {
+        window.location.href = 'https://x.com/home';
+    }
+    if (event.key === 'h' || event.key === 'H') {  
+        window.location.href = 'https://app.hubstaff.com/dashboard/559623/me';
+    }
+    if (event.key === 'w' || event.key === 'W') {
+        window.location.href = 'https://web.whatsapp.com/';
+    }
+    if (event.key === 'o' || event.key === 'O') {
+        window.location.href = 'https://open.spotify.com/';
+    }
+    if (event.key === 'c' || event.key === 'C') {
+        window.location.href = 'https://chatgpt.com/';
+    }
+    if (event.key === 'g' || event.key === 'G') { 
+        window.location.href = 'https://mail.google.com/mail/u/0/#inbox';
+    }
+});
